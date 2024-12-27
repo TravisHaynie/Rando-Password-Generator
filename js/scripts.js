947e726a3b5eda57eb87062d915d108d4b2a4c8f
unknown <travihay@gmail.com>
2024-12-26 22:49:54 -0600

    changed html js and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,35 @@
 let containerEl1 = document.getElementById("container-el1");
+console.log(containerEl1.value)
 let containerEl2 = document.getElementById("container-el2");
 let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
-let numberCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
 let passwordLength = 15;
 let passwordLengthEl = document.getElementById("password-length").value;
-let isClicked = true
-
+isClicked = true;
+console.log(containerEl1)
+console.log(containerEl1Value)
 
 function generateRandomPassword() {
+
     passwordLengthEl = document.getElementById("password-length").value;
-    passwordLength = 15
-
-
+    passwordLength = 15;;
 
     if (passwordLengthEl >= 1 && passwordLengthEl <= 23) {
-        passwordLength = passwordLengthEl
+        passwordLength = passwordLengthEl;
         containerEl1.textContent = randomPassword();
         containerEl2.textContent = randomPassword();
     } else {
         containerEl1.textContent = randomPassword();
         containerEl2.textContent = randomPassword();
     }
-
+    console.log(containerEl1)
+    
 }
 
 function randomPassword() {
+
     let password = "";
-    
+
     for (let i = 0; i < passwordLength; i++) {
         const randonPassword = Math.floor(Math.random() * characters.length)
         password += characters[randonPassword]
@@ -36,63 +38,52 @@ function randomPassword() {
 
 }
 
-function generateRandomNumberPassword() {
-    passwordLengthEl = document.getElementById("password-length").value;
-    passwordLength = 15
-
-
-    if (passwordLengthEl >= 1 && passwordLengthEl <= 23) {
-        passwordLength = passwordLengthEl
-        containerEl1.textContent = randomNumberPassword();
-        containerEl2.textContent = randomNumberPassword();
-    } else {
-        containerEl1.textContent = randomNumberPassword();
-        containerEl2.textContent = randomNumberPassword();
-    }
-
-}
-
 function randomNumberPassword() {
+
     let password = "";
 
-    
     for (let i = 0; i < passwordLength; i++) {
-        const randomPassword = Math.floor(Math.random() * characters.length)
-        password += randomPassword
+        const randonPassword = Math.floor(Math.random() * 10)
+        password += randonPassword
     }
     return password
-
 }
+
+
 
 function toggleBetweenNumbersOrSymbols() {
 
     if (isClicked) {
-      
         containerEl1.textContent = randomNumberPassword()
         containerEl2.textContent = randomNumberPassword()
-        isClicked = false
+
+        isClicked = false;
 
     } else {
+        containerEl1.textContent = randomPassword();
+        containerEl2.textContent = randomPassword();
 
-        passwordLengthEl = document.getElementById("password-length").value;
-        passwordLength = 15
-    
-        if (passwordLengthEl >= 1 && passwordLengthEl <= 23) {
-            passwordLength = passwordLengthEl
-            containerEl1.textContent = randomPassword();
-            containerEl2.textContent = randomPassword();
-        } else {
-            containerEl1.textContent = randomPassword();
-            containerEl2.textContent = randomPassword();
-        }
-
-        isClicked = true
-    } 
-
-    console.log(containerEl1)
+        isClicked = true;
+    }
 
 }
 
-console.log(randomNumberPassword())
+
+function copyContainerOne() {
+
+    let containerEl1 = document.getElementById("container-el1").textContent;
+    console.log(containerEl1)
+    navigator.clipboard.writeText(containerEl1);
+
+}
+
+
+function copyContainerTwo() {
+
+    let containerEl2 = document.getElementById("container-el2").textContent;
+    navigator.clipboard.writeText(containerEl2);
+    console.log(containerEl2)
+
+}
 
 
