c7c5608d0b2764fffb4dd61184b68e94965f8734
unknown <travihay@gmail.com>
2024-12-28 13:57:27 -0600

    changed the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,39 @@
 let containerEl1 = document.getElementById("container-el1");
-console.log(containerEl1.value)
 let containerEl2 = document.getElementById("container-el2");
 let characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
 let passwordLength = 15;
 let passwordLengthEl = document.getElementById("password-length").value;
 isClicked = true;
-console.log(containerEl1)
-console.log(containerEl1Value)
+let getRandomPassword = "";
+let getRandomPassword2 = "";
+let getRandomNumberPassword = "";
+let getRandomNumberPassword2 = "";
 
 function generateRandomPassword() {
-
     passwordLengthEl = document.getElementById("password-length").value;
-    passwordLength = 15;;
+    passwordLength = 15;
 
-    if (passwordLengthEl >= 1 && passwordLengthEl <= 23) {
+    getRandomPassword = randomPassword()
+    getRandomPassword2 = randomPassword()
+    containerEl1.textContent = getRandomPassword;
+    containerEl2.textContent = getRandomPassword2;
+
+    if (passwordLengthEl > 0 && passwordLengthEl <= 23) {
         passwordLength = passwordLengthEl;
-        containerEl1.textContent = randomPassword();
-        containerEl2.textContent = randomPassword();
-    } else {
-        containerEl1.textContent = randomPassword();
-        containerEl2.textContent = randomPassword();
+        getRandomPassword = randomPassword()
+        getRandomPassword2 = randomPassword()
+        containerEl1.textContent = getRandomPassword;
+        containerEl2.textContent = getRandomPassword2;
     }
-    console.log(containerEl1)
-    
+
+    getRandomNumberPassword = randomNumberPassword()
+    getRandomNumberPassword2 = randomNumberPassword()
+
+
 }
 
 function randomPassword() {
-
     let password = "";
 
     for (let i = 0; i < passwordLength; i++) {
@@ -39,7 +45,6 @@ function randomPassword() {
 }
 
 function randomNumberPassword() {
-
     let password = "";
 
     for (let i = 0; i < passwordLength; i++) {
@@ -52,38 +57,30 @@ function randomNumberPassword() {
 
 
 function toggleBetweenNumbersOrSymbols() {
-
     if (isClicked) {
-        containerEl1.textContent = randomNumberPassword()
-        containerEl2.textContent = randomNumberPassword()
+        containerEl1.textContent = getRandomNumberPassword
+        containerEl2.textContent = getRandomNumberPassword2
 
         isClicked = false;
 
     } else {
-        containerEl1.textContent = randomPassword();
-        containerEl2.textContent = randomPassword();
+        containerEl1.textContent = getRandomPassword;
+        containerEl2.textContent = getRandomPassword2;
 
         isClicked = true;
     }
 
 }
 
-
 function copyContainerOne() {
-
     let containerEl1 = document.getElementById("container-el1").textContent;
-    console.log(containerEl1)
     navigator.clipboard.writeText(containerEl1);
-
 }
 
 
 function copyContainerTwo() {
-
     let containerEl2 = document.getElementById("container-el2").textContent;
     navigator.clipboard.writeText(containerEl2);
-    console.log(containerEl2)
-
 }
 
 
